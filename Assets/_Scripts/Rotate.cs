67f6c77b07f220bfad1ac4417e92e8cb3d9b00e5
Matthew Page <Matt@MattsMacbookPro.local>
2016-06-02 08:53:05 +0100

    Initial Commit

---
+++ Assets/_Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, speed * Time.deltaTime, 0, Space.Self);
+	}
+}
